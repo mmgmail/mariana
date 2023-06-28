e532d93846570bf2dc861ddc08c2525f2e8d903c
mmgmail <qwerty.qq185@gmail.com>
2023-06-28 23:09:37 +0300

    size

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import LeftBar from '../LeftBar';
 import RightBar from '../RightBar';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -11,7 +8,6 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 
 import GoodsCtx from '../../contexts/goodsCtx';
@@ -21,73 +17,109 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
     id: 0,
-    price: 10000
+    price: 10000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
     id: 1,
-    price: 20000
+    price: 20000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
     title: 'Camera',
     id: 2,
-    price: 5000
+    price: 5000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
     id: 3,
-    price: 15000
+    price: 15000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
     id: 4,
-    price: 25000
+    price: 25000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
     id: 5,
-    price: 12000
+    price: 12000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
     title: 'Basketball',
     id: 6,
-    price: 13000
+    price: 13000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
     title: 'Fern',
     id: 7,
-    price: 21000
+    price: 21000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
     title: 'Mushrooms',
     id: 8,
-    price: 22000
+    price: 22000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
     title: 'Tomato basil',
     id: 9,
-    price: 20100
+    price: 20100,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
     title: 'Sea star',
     id: 10,
-    price: 18000
+    price: 18000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
   {
     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
     title: 'Bike',
     id: 11,
-    price: 7000
+    price: 7000,
+    width: 1200,
+    length: 3200,
+    height: 1400,
   },
 ];
 
@@ -97,6 +129,9 @@ export default function MainLayout() {
   const [images, setImages] = React.useState([]);
   const goodsCtx = React.useContext(GoodsCtx);
   const [cost, setCost] = React.useState(0);
+  const [width, setWidth] = React.useState(0);
+  const [length, setLength] = React.useState(0);
+  const [height, setHeight] = React.useState(0);
 
   const handleDragOver= (e) => {
     e.stopPropagation();
@@ -129,12 +164,24 @@ export default function MainLayout() {
   React.useEffect(() => {
     goodsCtx.items = images;
     let arrSum = [];
+    let arrWidth = [];
+    let arrLength = [];
+    let arrHeight = [];
     for (let i = 0; i < images.length; i++) {
       arrSum.push(+images[i]?.src?.item?.price);
+      arrWidth.push(+images[i]?.src?.item?.width);
+      arrLength.push(+images[i]?.src?.item?.length);
+      arrHeight.push(+images[i]?.src?.item?.height);
     }
 
-    const sum = arrSum.reduce((partialSum, a) => partialSum + a, 0);
-    setCost(sum);
+    const sumCost = arrSum.reduce((partialSum, a) => partialSum + a, 0);
+    const sumWidth = arrWidth.reduce((partialSum, a) => partialSum + a, 0);
+    const sumLength = arrLength.reduce((partialSum, a) => partialSum + a, 0);
+    const sumHeight = arrHeight.reduce((partialSum, a) => partialSum + a, 0);
+    setCost(sumCost);
+    setWidth(sumWidth);
+    setLength(sumLength);
+    setHeight(sumHeight);
 
   }, [JSON.stringify(images)])
 
@@ -145,15 +192,13 @@ export default function MainLayout() {
           <RightBar images={images} setImages={setImages} stageRef={stageRef} onDragOver={handleDragOver} onDrop={onDropHandler} />
         </Grid>
         <Grid item xs={12} md={3}>
-          {/* <LeftBar onDragStart={handledragStart} itemData={itemData} /> */}
-          
           <Accordion>
             <AccordionSummary
               // expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
               id="panel1a-header"
             >
-              <Typography>Configuration</Typography>
+              <Typography>Sofa configuration</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <ImageList sx={{ height: 550 }} cols={2} rowHeight={164}>
@@ -175,7 +220,16 @@ export default function MainLayout() {
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
               <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                COST
+                SOFA SIZE:
+              </Typography>
+              <Typography variant="h6" component="div">
+                Width: {width} sm <br />
+                Length: {length} sm <br />
+                Height: {height} sm
+              </Typography>
+              <hr />
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                COST:
               </Typography>
               <Typography variant="h5" component="div">
                 {cost} UAH
