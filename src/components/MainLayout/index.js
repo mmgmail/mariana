3d8300a5c3babf1d9955081790b03acc1e45fd62
mmgmail <qwerty.qq185@gmail.com>
2023-06-28 16:09:25 +0300

    wip transform

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -11,50 +11,62 @@ const itemData = [
   {
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
+    id: 0,
   },
   {
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
+    id: 1,
   },
   {
     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
     title: 'Camera',
+    id: 2,
   },
   {
     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
+    id: 3,
   },
   {
     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
+    id: 4,
   },
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
+    id: 5,
   },
   {
     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
     title: 'Basketball',
+    id: 6,
   },
   {
     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
     title: 'Fern',
+    id: 7,
   },
   {
     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
     title: 'Mushrooms',
+    id: 8,
   },
   {
     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
     title: 'Tomato basil',
+    id: 9,
   },
   {
     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
     title: 'Sea star',
+    id: 10,
   },
   {
     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
     title: 'Bike',
+    id: 11,
   },
 ];
 
@@ -69,12 +81,13 @@ export default function MainLayout() {
   }
 
   const handledragStart = (e, idx) => {
-    console.log('e2', e)
-    dragUrl.current = { src: e.target.src, idx };
+    // console.log('e2', dragUrl.current)
+    const id = "id" + Math.random().toString(16).slice(2);
+    dragUrl.current = { src: e.target.src, idx, id  };
   }
 
   const onDropHandler = (e) => {
-    console.log('e3', e)
+    console.log('e3', dragUrl.current)
     e.stopPropagation();
     e.preventDefault();
     // register event position
@@ -94,7 +107,7 @@ export default function MainLayout() {
     <React.Fragment>
       <Grid container spacing={3}>
         <Grid item xs={12} md={9}>
-          <RightBar images={images} stageRef={stageRef} onDragOver={handleDragOver} onDrop={onDropHandler} />
+          <RightBar images={images} setImages={setImages} stageRef={stageRef} onDragOver={handleDragOver} onDrop={onDropHandler} />
         </Grid>
         <Grid item xs={12} md={3}>
           {/* <LeftBar onDragStart={handledragStart} itemData={itemData} /> */}
--- src/components/RightBar/index.js
+++ src/components/RightBar/index.js
@@ -1,44 +1,132 @@
 
 import * as React from 'react';
-import { Stage, Layer, Image, Group, Rect, Util } from 'react-konva';
+import { Stage, Layer, Image, Group, Rect, Transformer, Util } from 'react-konva';
 import useImage from 'use-image';
 
-const URLImage = ({ image, refShape, refGroup }) => {
-  const [img] = useImage(image.src.src);
+const URLImage = ({ image, refShape, refLayer, groupProps, isSelected, onSelect, onChange, onDragStart }) => {
+  console.log('image', image)
+  const [img] = useImage(image?.src?.src);
+  const trRef = React.useRef();
+  const refGroup = React.useRef(null);
+
+  // function updateSelectionBox() {
+  //   console.log(' refShape.current',  refShape.current)
+  //   refShape.current.visible(true);
+  //   // refShape.current.width(refGroup.current.width());
+  //   // refShape.current.height(refGroup.current.height());
+  //   // refShape.current.position({
+  //   //   x: refGroup.current.x(),
+  //   //   y: refGroup.current.y()
+  //   // });
+  //   // refShape.current.rotation(refGroup.current.rotation());
+  // }
+
+  function hideSelectionBox() {
+    refShape.current.visible(false);
+  }
+
+  const handleTransform = e => {
+    const currentRotation = refGroup.current.rotation();
+    const nearestRotation = Math.round(currentRotation / 45) * 45;
+    refGroup.current.rotation(nearestRotation);
+  };
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([refGroup.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
   return (
-    <Group
-      x={image.x}
-      y={image.y}
-      offsetX={img ? img.width / 2 : 0}
-      offsetY={img ? img.height / 2 : 0}
-      draggable={true}
-      ref={refGroup}
-    >
-      <Rect
-        width={img ? img.width : 0}
-        height={img ? img.height : 0}
-        fill={'grey'}
-        ref={refShape}
-      />
+    <React.Fragment>
+      <Group
+        x={image.x}
+        y={image.y}
+        offsetX={img ? img.width / 2 : 0}
+        offsetY={img ? img.height / 2 : 0}
+        draggable={true}
+        ref={refGroup}
+        {...groupProps}
+        onClick={onSelect}
+        onTap={onSelect}
+        onDragStart={onDragStart}
+        onDragEnd={(e) => {
+          onChange({
+            ...groupProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = refGroup.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...groupProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            // width: Math.max(node.width() * scaleX),
+            // height: Math.max(node.height() * scaleY),
+          });
+        }}
+      >
+        {/* <Rect
+          width={img ? img.width : 0}
+          height={img ? img.height : 0}
+          ref={refShape}
+          strokeWidth={3}
+          stroke={'red'}
+          visible={false}
+        /> */}
         <Image
           image={img}
-          
           shadowColor={'black'}
         />
-      
-    </Group>
+        
+      </Group>
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          resizeEnabled={false}
+          anchorSize={20}
+          borderDash={[3, 3]}
+          transformerConfig={{
+            rotationSnaps: [0, 45, 90, 135, 180, 225, 270, 315],
+          }}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
-export default function RightBar({ stageRef, images, onDragOver, onDrop }) {
-  const [dragging, setDragging] = React.useState(false);
+export default function RightBar({ stageRef, images, onDragOver, onDrop, setImages }) {
+  const [rectangles, setRectangles] = React.useState(images);
+  const [detectedSide, setDetectedSide] = React.useState(false);
+  const [selectedId, selectShape] = React.useState(null);
+
   const refLayer = React.useRef(null);
-  const refGroup = React.useRef(null);
-  const refShape = React.useRef(null);
-  let detectedSide;
+  
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
 
   const handlerDragMove = (e) => {
-    setDragging(true);
+    setDetectedSide(null);
     const target = e.target;
     const targetRect = e.target.getClientRect();
     const x1 = targetRect.x;
@@ -49,6 +137,10 @@ export default function RightBar({ stageRef, images, onDragOver, onDrop }) {
     const h1 = targetRect.height;
 
     refLayer.current.children.forEach(function (group) {
+      if (group === target) {
+        return;
+      }
+
       const groupRect = group.getClientRect();
       const x2 = groupRect.x;
       const y2 = groupRect.y;
@@ -56,56 +148,52 @@ export default function RightBar({ stageRef, images, onDragOver, onDrop }) {
       const oy2 = group.attrs.offsetY;
       const w2 = groupRect.width;
       const h2 = groupRect.height;
+      // console.log('group', group);
     
       if (x1 + w1 < x2) {
-        detectedSide = 'left';
+        setDetectedSide('left');
       } else if (x1 > x2 + w2) {
-        detectedSide = 'right';
+        setDetectedSide('right');
       } else if (y1 + h1 < y2) {
-        detectedSide = 'top';
+        setDetectedSide('top');
       } else if (y1 > y2 + h2) {
-        detectedSide = 'bottom';
+        setDetectedSide('bottom');
       } else {
         // console.log('intersecting')
       }
       // console.log(detectedSide)
-  
-      if (group === target) {
-        return;
-      }
+
       if (haveIntersection(group.getClientRect(), targetRect)) {
-        setDragging(false);
-        
-        switch(detectedSide) {
-          case 'left': {
-            target.to({
-              x: x2 - ox2,
-              y: y2 + oy2,
-            })
-            break;
-          }
-          case 'right': {
-            target.to({
-              x: x2 + ox2 + w1,
-              y: y2 + oy2,
-            })
-            break;
-          }
-          case 'top': {
-            target.to({
-              x: x2 + ox2,
-              y: y2 - oy2,
-            })
-            break;
-          }
-          case 'bottom': {
-            target.to({
-              x: x2 + ox2,
-              y: y2 + h1 + oy1,
-            })
-            break;
-          }
-        }
+        // switch(detectedSide) {
+        //   case 'left': {
+        //     target.to({
+        //       x: x2 - ox2,
+        //       y: y2 + oy2,
+        //     })
+        //     break;
+        //   }
+        //   case 'right': {
+        //     target.to({
+        //       x: x2 + ox2 + w1,
+        //       y: y2 + oy2,
+        //     })
+        //     break;
+        //   }
+        //   case 'top': {
+        //     target.to({
+        //       x: x2 + ox2,
+        //       y: y2 - oy2,
+        //     })
+        //     break;
+        //   }
+        //   case 'bottom': {
+        //     target.to({
+        //       x: x2 + ox2,
+        //       y: y2 + h1 + oy1,
+        //     })
+        //     break;
+        //   }
+        // }
         
         // group.children[0].attrs.fill = 'red'
       } else {
@@ -130,18 +218,29 @@ export default function RightBar({ stageRef, images, onDragOver, onDrop }) {
         height={800}
         style={{ border: '1px solid grey', backgroundColor: 'lightGrey' }}
         ref={stageRef}
+        onMouseDown={checkDeselect}
+        onTouchStart={checkDeselect}
       >
         <Layer
           onDragMove={handlerDragMove}
           ref={refLayer}
         >
-          {images.map((image) => {
+          {images.map((image, i) => {
             return <URLImage
-              key={image.src.src}
+              key={i}
               image={image}
               stageRef={stageRef}
-              refShape={refShape}
-              refGroup={refGroup}
+              refLayer={refLayer}
+              shapeProps={image}
+              isSelected={image.src.id === selectedId}
+              onSelect={(e) => {
+                selectShape(image?.src?.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = images.slice();
+                rects[i] = {...image, ...newAttrs};
+                setImages(rects);
+              }}
             />;
           })}
         </Layer>
