1fd9917fb9972cd8728bceacd45271c56b19f68c
mmgmail <qwerty.qq185@gmail.com>
2023-07-11 22:04:36 +0300

    wip json

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -9,119 +9,8 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-
 import GoodsCtx from '../../contexts/goodsCtx';
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    id: 0,
-    price: 10000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    id: 1,
-    price: 20000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    id: 2,
-    price: 5000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    id: 3,
-    price: 15000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    id: 4,
-    price: 25000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    id: 5,
-    price: 12000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    id: 6,
-    price: 13000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    id: 7,
-    price: 21000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    id: 8,
-    price: 22000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    id: 9,
-    price: 20100,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    id: 10,
-    price: 18000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    id: 11,
-    price: 7000,
-    width: 1200,
-    length: 3200,
-    height: 1400,
-  },
-];
+import goodsJson from '../../services/json/goods.json';
 
 export default function MainLayout() {
   const dragUrl = React.useRef({ src: null, idx: -1 });
@@ -132,6 +21,7 @@ export default function MainLayout() {
   const [width, setWidth] = React.useState(0);
   const [length, setLength] = React.useState(0);
   const [height, setHeight] = React.useState(0);
+  const [data, setData] = React.useState(null);
 
   const handleDragOver= (e) => {
     e.stopPropagation();
@@ -202,18 +92,23 @@ export default function MainLayout() {
             </AccordionSummary>
             <AccordionDetails>
               <ImageList sx={{ height: 550 }} cols={2} rowHeight={164}>
-                {itemData.map((item, idx) => (
-                  <ImageListItem key={item.img}>
-                    <img
-                      src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                      srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                      alt={item.title}
-                      loading="lazy"
-                      draggable="true"
-                      onDragStart={(e) => handledragStart(e, idx, item)}
-                    />
-                  </ImageListItem>
-                ))}
+                  {goodsJson?.goods?.map((item, idx) => {
+                    const Image = React.lazy(() => import(item.img));
+                    return (
+                      <ImageListItem key={item.img}>
+                        <Image
+                          // src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                          // src={`${item.img}`}
+                          srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                          alt={item.title}
+                          loading="lazy"
+                          draggable="true"
+                          onDragStart={(e) => handledragStart(e, idx, item)}
+                        />
+                      </ImageListItem>
+                    )
+                  })
+                }
               </ImageList>
             </AccordionDetails>
           </Accordion>
